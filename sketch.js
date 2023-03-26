94e29f5c3d45e36c0f3244749a2821cf4862118c
victor145646 <128987183+victor145646@users.noreply.github.com>
2023-03-26 12:11:06 -0300

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,23 @@
+//crie as variaveis dos seus personagens aqui
+var box;
+
+
+function preload(){
+  //carregue as imagens dos personagens aqui
+}
+
+function setup() {
+  createCanvas(800,400);
+  //crie os sprites aqui
+  box = createSprite(200,200,50,80);
+  box.shapeColor = "red";
+
+  
+
+}
+
+function draw() {
+  background(255,255,255); 
+  
+  drawSprites();
+}
